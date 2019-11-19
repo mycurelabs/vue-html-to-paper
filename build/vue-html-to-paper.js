a205c71062c9d38ee7fde69952f5ca005da91561
Celeste <yuan.zx@outlook.com>
2019-11-19 20:18:46 +0800

    chore: create new build

--- build/vue-html-to-paper.js
+++ build/vue-html-to-paper.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, global.VueHtmlToPaper = factory());
-}(this, function () { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global.VueHtmlToPaper = {}));
+}(this, function (exports) { 'use strict';
 
   function addStyles (win, styles) {
     styles.forEach(style => {
@@ -14,56 +14,72 @@
     });
   }
 
+  const options = {
+    name: '_blank',
+    specs: ['fullscreen=yes', 'titlebar=yes', 'scrollbars=yes'].join(','),
+    replace: true,
+    styles: []
+  };
+
+  const setOptions = ({ name, specs, replace, styles }) => {
+    specs = !!specs.length ? specs.join(',') : '';
+
+    options.name = name || options.name;
+    options.specs = specs || options.specs;
+    options.replace = replace || options.replace;
+    options.styles = styles || options.styles;
+  };
+
+  const htmlToPaper = (el, cb = () => true) => {
+    const element = document.getElementById(el);
+
+    if(!element) {
+      alert(`Element to print #${el} not found!`);
+      return;
+    }
+
+    const url = '';
+    const win = window.open(url, options.name, options.specs, options.replace);
+
+    win.document.write(`
+    <html>
+      <head>
+        <title>${document.title}</title>
+      </head>
+      <body>
+        ${element.innerHTML}
+      </body>
+    </html>
+  `);
+
+    addStyles(win, options.styles);
+
+    setTimeout(() => {
+      win.document.close();
+      win.focus();
+      win.print();
+      win.close();
+      cb();
+    }, 1000);
+    return true;
+  };
+
   const VueHtmlToPaper = {
-    install (Vue, options = {}) {
+    install(Vue, options = {}) {
+      setOptions(options);
+
       Vue.mixin({
         methods: {
-          $htmlToPaper (el, cb = () => true) {
-            let {
-              name = '_blank',
-              specs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
-              replace = true,
-              styles = []
-            } = options;
-            specs = !!specs.length ? specs.join(',') : '';
-
-            const element = document.getElementById(el);
-
-            if(!element) {
-              alert(`Element to print #${el} not found!`);
-              return;
-            }
-            
-            const url = '';
-            const win = window.open(url, name, specs, replace);
-
-            win.document.write(`
-            <html>
-              <head>
-                <title>${document.title}</title>
-              </head>
-              <body>
-                ${element.innerHTML}
-              </body>
-            </html>
-          `);
-
-            addStyles(win, styles);
-            
-            setTimeout(() => {
-              win.document.close();
-              win.focus();
-              win.print();
-              win.close();
-              cb();
-            }, 1000);          
-            return true;
-          }
+          $htmlToPaper: htmlToPaper
         }
       });
     }
   };
 
-  return VueHtmlToPaper;
+  exports.default = VueHtmlToPaper;
+  exports.htmlToPaper = htmlToPaper;
+  exports.setOptions = setOptions;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
 
 }));
