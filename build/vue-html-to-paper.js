2b2d60ccd7150120d135ba76373454fddd45cb23
Celeste <yuan.zx@outlook.com>
2020-02-13 21:25:12 +0800

    feat: use without adding instance properties
    - export htmlToPaper func
    - improve option logic
    
    close #29

--- build/vue-html-to-paper.js
+++ build/vue-html-to-paper.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, global.VueHtmlToPaper = factory());
-}(this, function () { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global.VueHtmlToPaper = {}));
+}(this, function (exports) { 'use strict';
 
   function addStyles(win, styles) {
     styles.forEach(style => {
@@ -21,69 +21,69 @@
     styles: []
   };
 
-  const getOrDefault = (local, global) => field =>
-    local[field]
-      ? local[field]
-      : global[field]
-      ? global[field]
-      : defaultOptions[field];
+  function setOptions(options) {
+    Object.assign(defaultOptions, options);
+  }
+
+  function htmlToPaper(el, localOptions, cb) {
+    const options = Object.assign({}, defaultOptions, localOptions);
+    let { name, replace, styles, specs } = options;
+
+    if (process.env.NODE_ENV !== "production") {
+      console.warn(styles);
+    }
+
+    specs = !!specs.length ? specs.join(",") : "";
+
+    const element = document.getElementById(el);
+
+    if (!element) {
+      alert(`Element to print #${el} not found!`);
+      return cb ? undefined : Promise.reject()
+    }
+
+    const url = "";
+    const win = window.open(url, name, specs, replace);
+
+    win.document.write(
+      [
+        `<html>`,
+        `<head>`,
+        `<title>${document.title}</title>`,
+        `</head>`,
+        `<body>${element.innerHTML}</body>`,
+        `</html>`
+      ].join("")
+    );
+
+    addStyles(win, styles);
+
+    const promise = new Promise(resolve => {
+      setTimeout(() => {
+        win.document.close();
+        win.focus();
+        win.print();
+        win.close();
+
+        if (cb) cb();
+        resolve();
+      }, 1000);
+    });
+
+    return cb ? true : promise
+  };
 
   const VueHtmlToPaper = {
-    install(Vue, options = {}) {
-      Vue.prototype.$htmlToPaper = (el, localOptions, cb) => {
-        // If has localOptions
-        const get = getOrDefault(localOptions, options);
-        const name = get("name");
-        const replace = get("replace");
-        const styles = get("styles");
-        let specs = get("specs");
-
-        if (process.env.NODE_ENV !== "production") {
-          console.warn(styles);
-        }
-
-        specs = !!specs.length ? specs.join(",") : "";
-
-        const element = document.getElementById(el);
-
-        if (!element) {
-          alert(`Element to print #${el} not found!`);
-          return cb ? undefined : Promise.reject()
-        }
-
-        const url = "";
-        const win = window.open(url, name, specs, replace);
-
-        win.document.write(
-          [
-            `<html>`,
-            `<head>`,
-            `<title>${document.title}</title>`,
-            `</head>`,
-            `<body>${element.innerHTML}</body>`,
-            `</html>`
-          ].join("")
-        );
-
-        addStyles(win, styles);
-
-        const promise = new Promise(resolve => {
-          setTimeout(() => {
-            win.document.close();
-            win.focus();
-            win.print();
-            win.close();
-
-            if (cb) cb();
-            resolve();
-          }, 1000);
-        });
-
-        return cb ? true : promise
-      };
+    install(Vue, options) {
+      setOptions(options);
+      Vue.prototype.$htmlToPaper = htmlToPaper;
     }
   };
 
-  return VueHtmlToPaper;
+  exports.default = VueHtmlToPaper;
+  exports.htmlToPaper = htmlToPaper;
+  exports.setOptions = setOptions;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
 
 }));
--- src/index.js
+++ src/index.js
@@ -15,67 +15,64 @@ const defaultOptions = {
   styles: []
 }
 
-const getOrDefault = (local, global) => field =>
-  local[field]
-    ? local[field]
-    : global[field]
-    ? global[field]
-    : defaultOptions[field]
+function setOptions(options) {
+  Object.assign(defaultOptions, options)
+}
+
+function htmlToPaper(el, localOptions, cb) {
+  const options = Object.assign({}, defaultOptions, localOptions)
+  let { name, replace, styles, specs } = options
+
+  if (process.env.NODE_ENV !== "production") {
+    console.warn(styles)
+  }
+
+  specs = !!specs.length ? specs.join(",") : ""
+
+  const element = document.getElementById(el)
+
+  if (!element) {
+    alert(`Element to print #${el} not found!`)
+    return cb ? undefined : Promise.reject()
+  }
+
+  const url = ""
+  const win = window.open(url, name, specs, replace)
+
+  win.document.write(
+    [
+      `<html>`,
+      `<head>`,
+      `<title>${document.title}</title>`,
+      `</head>`,
+      `<body>${element.innerHTML}</body>`,
+      `</html>`
+    ].join("")
+  )
+
+  addStyles(win, styles)
+
+  const promise = new Promise(resolve => {
+    setTimeout(() => {
+      win.document.close()
+      win.focus()
+      win.print()
+      win.close()
+
+      if (cb) cb()
+      resolve()
+    }, 1000)
+  })
+
+  return cb ? true : promise
+}
 
 const VueHtmlToPaper = {
-  install(Vue, options = {}) {
-    Vue.prototype.$htmlToPaper = (el, localOptions, cb) => {
-      // If has localOptions
-      const get = getOrDefault(localOptions, options)
-      const name = get("name")
-      const replace = get("replace")
-      const styles = get("styles")
-      let specs = get("specs")
-
-      if (process.env.NODE_ENV !== "production") {
-        console.warn(styles)
-      }
-
-      specs = !!specs.length ? specs.join(",") : ""
-
-      const element = document.getElementById(el)
-
-      if (!element) {
-        alert(`Element to print #${el} not found!`)
-        return cb ? undefined : Promise.reject()
-      }
-
-      const url = ""
-      const win = window.open(url, name, specs, replace)
-
-      win.document.write(
-        [
-          `<html>`,
-          `<head>`,
-          `<title>${document.title}</title>`,
-          `</head>`,
-          `<body>${element.innerHTML}</body>`,
-          `</html>`
-        ].join("")
-      )
-
-      addStyles(win, styles)
-
-      const promise = new Promise(resolve => {
-        setTimeout(() => {
-          win.document.close()
-          win.focus()
-          win.print()
-          win.close()
-
-          if (cb) cb()
-          resolve()
-        }, 1000)
-      })
-
-      return cb ? true : promise
-    }
+  install(Vue, options) {
+    setOptions(options)
+    Vue.prototype.$htmlToPaper = htmlToPaper
   }
 }
 
 export default VueHtmlToPaper
+export { htmlToPaper, setOptions }
