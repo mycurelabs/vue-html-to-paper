b2dc4bbe119acd4f6bee2e6cc6cafc9fa16a3b87
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-05-05 09:13:53 +0800

    chore: new build

--- build/vue-html-to-paper.js
+++ build/vue-html-to-paper.js
@@ -1,45 +1,38 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, global.VueHtmlToPaper = factory());
-}(this, function () { 'use strict';
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.VueHtmlToPaper = factory());
+}(this, (function () { 'use strict';
 
-  function addStyles (win, styles) {
-    styles.forEach(style => {
+  const addStyles = (win, styles) => {
+    styles.forEach((style) => {
       let link = win.document.createElement('link');
       link.setAttribute('rel', 'stylesheet');
       link.setAttribute('type', 'text/css');
       link.setAttribute('href', style);
       win.document.getElementsByTagName('head')[0].appendChild(link);
     });
-  }
+  };
 
   const VueHtmlToPaper = {
     install (Vue, options = {}) {
       Vue.prototype.$htmlToPaper = (el, localOptions, cb = () => true) => {
         let defaultName = '_blank', 
-            defaultSpecs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
-            defaultReplace = true,
-            defaultStyles = [];
+          defaultSpecs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
+          defaultStyles = [];
         let {
           name = defaultName,
           specs = defaultSpecs,
-          replace = defaultReplace,
-          styles = defaultStyles
+          styles = defaultStyles,
         } = options;
 
-        // If has localOptions
-        // TODO: improve logic
-        if (!!localOptions) {
-          if (localOptions.name) name = localOptions.name;
-          if (localOptions.specs) specs = localOptions.specs;
-          if (localOptions.replace) replace = localOptions.replace;
-          if (localOptions.styles) styles = localOptions.styles;
-        }
+        if (localOptions?.name) name = localOptions.name;
+        if (localOptions?.specs) specs = localOptions.specs;
+        if (localOptions?.styles) styles = localOptions.styles;
 
         specs = !!specs.length ? specs.join(',') : '';
 
-        const element = document.getElementById(el);
+        const element = window.document.getElementById(el);
 
         if (!element) {
           alert(`Element to print #${el} not found!`);
@@ -47,12 +40,12 @@
         }
         
         const url = '';
-        const win = window.open(url, name, specs, replace);
+        const win = window.open(url, name, specs);
 
         win.document.write(`
         <html>
           <head>
-            <title>${document.title}</title>
+            <title>${window.document.title}</title>
           </head>
           <body>
             ${element.innerHTML}
@@ -72,9 +65,9 @@
           
         return true;
       };
-    }
+    },
   };
 
   return VueHtmlToPaper;
 
-}));
+})));
