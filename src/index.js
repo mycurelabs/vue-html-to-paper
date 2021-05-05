295ceb580e1437595db1734bb5946705e2ce2be4
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-05-05 09:15:41 +0800

    refactor: improve code
    - add optional chaining
    - fix lint errors
    - remove 4th param in window.open see https://github.com/mycurelabs/vue-html-to-paper/issues/77#issuecomment-815502388

--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
-function addStyles (win, styles) {
-  styles.forEach(style => {
+const addStyles = (win, styles) => {
+  styles.forEach((style) => {
     let link = win.document.createElement('link');
     link.setAttribute('rel', 'stylesheet');
     link.setAttribute('type', 'text/css');
     link.setAttribute('href', style);
     win.document.getElementsByTagName('head')[0].appendChild(link);
   });
-}
+};
 
 const VueHtmlToPaper = {
   install (Vue, options = {}) {
     Vue.prototype.$htmlToPaper = (el, localOptions, cb = () => true) => {
       let defaultName = '_blank', 
-          defaultSpecs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
-          defaultReplace = true,
-          defaultStyles = []
+        defaultSpecs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
+        defaultStyles = [];
       let {
         name = defaultName,
         specs = defaultSpecs,
-        replace = defaultReplace,
-        styles = defaultStyles
+        styles = defaultStyles,
       } = options;
 
-      // If has localOptions
-      // TODO: improve logic
-      if (!!localOptions) {
-        if (localOptions.name) name = localOptions.name;
-        if (localOptions.specs) specs = localOptions.specs;
-        if (localOptions.replace) replace = localOptions.replace;
-        if (localOptions.styles) styles = localOptions.styles;
-      }
+      if (localOptions?.name) name = localOptions.name;
+      if (localOptions?.specs) specs = localOptions.specs;
+      if (localOptions?.styles) styles = localOptions.styles;
 
       specs = !!specs.length ? specs.join(',') : '';
 
@@ -41,7 +34,7 @@ const VueHtmlToPaper = {
       }
       
       const url = '';
-      const win = window.open(url, name, specs, replace);
+      const win = window.open(url, name, specs);
 
       win.document.write(`
         <html>
@@ -66,7 +59,7 @@ const VueHtmlToPaper = {
         
       return true;
     };
-  }
-}
+  },
+};
 
 export default VueHtmlToPaper;
