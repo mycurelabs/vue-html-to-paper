6f955e8f3246eabc9651b27eada467f944a6b422
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-10-31 21:25:53 +0800

    refactor: update code formatting

--- src/index.js
+++ src/index.js
@@ -1,86 +1,86 @@
 function addStyles (win, styles) {
-    styles.forEach(style => {
-      let link = win.document.createElement('link');
-      link.setAttribute('rel', 'stylesheet');
-      link.setAttribute('type', 'text/css');
-      link.setAttribute('href', style);
-      win.document.getElementsByTagName('head')[0].appendChild(link);
-    });
-  }
+  styles.forEach(style => {
+    let link = win.document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('type', 'text/css');
+    link.setAttribute('href', style);
+    win.document.getElementsByTagName('head')[0].appendChild(link);
+  });
+}
 
+function openWindow (url, name, props) {
+  let windowRef = null;
+  if (/*@cc_on!@*/false) { // for IE only
+    windowRef = window.open('', name, props);
+    windowRef.close();
+  }
+  windowRef = window.open(url, name, props);
+  if (!windowRef.opener) {
+    windowRef.opener = self;
+  }
+  windowRef.focus();
+  return windowRef;
+}
+  
+const VueHtmlToPaper = {
+  install (Vue, options = {}) {
+    Vue.prototype.$htmlToPaper = (el, localOptions, cb = () => true) => {
+      let defaultName = '_blank', 
+        defaultSpecs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
+        defaultReplace = true,
+        defaultStyles = [];
+      let {
+        name = defaultName,
+        specs = defaultSpecs,
+        replace = defaultReplace,
+        styles = defaultStyles,
+      } = options;
 
-  function openWindow(url, name, props) {
-    if(/*@cc_on!@*/false){ //do this only in IE
-      var windowRef = window.open("", name, props);
-      windowRef.close();
-    }
-    var windowRef = window.open(url, name, props);
-    if (!windowRef.opener) {
-      windowRef.opener = self;
-    }
-    windowRef.focus();
-    return windowRef;
-  }
-  
-  const VueHtmlToPaper = {
-    install (Vue, options = {}) {
-      Vue.prototype.$htmlToPaper = (el, localOptions, cb = () => true) => {
-        let defaultName = '_blank', 
-            defaultSpecs = ['fullscreen=yes','titlebar=yes', 'scrollbars=yes'],
-            defaultReplace = true,
-            defaultStyles = []
-        let {
-          name = defaultName,
-          specs = defaultSpecs,
-          replace = defaultReplace,
-          styles = defaultStyles
-        } = options;
-  
-        // If has localOptions
-        // TODO: improve logic
-        if (!!localOptions) {
-          if (localOptions.name) name = localOptions.name;
-          if (localOptions.specs) specs = localOptions.specs;
-          if (localOptions.replace) replace = localOptions.replace;
-          if (localOptions.styles) styles = localOptions.styles;
-        }
-  
-        specs = !!specs.length ? specs.join(',') : '';
-  
-        const element = window.document.getElementById(el);
-  
-        if (!element) {
-          alert(`Element to print #${el} not found!`);
-          return;
-        }
+      // If has localOptions
+      // TODO: improve logic
+      if (!!localOptions) {
+        if (localOptions.name) name = localOptions.name;
+        if (localOptions.specs) specs = localOptions.specs;
+        if (localOptions.replace) replace = localOptions.replace;
+        if (localOptions.styles) styles = localOptions.styles;
+      }
+
+      specs = !!specs.length ? specs.join(',') : '';
+
+      const element = window.document.getElementById(el);
+
+      if (!element) {
+        alert(`Element to print #${el} not found!`);
+        return;
+      }
+      
+      const url = '';
+      const win = openWindow(url, name, specs);
+
+      win.document.write(`
+        <html>
+          <head>
+            <title>${window.document.title}</title>
+          </head>
+          <body>
+            ${element.innerHTML}
+          </body>
+        </html>
+      `);
+
+      addStyles(win, styles);
+      
+      setTimeout(() => {
+        win.document.close();
+        win.focus();
+        win.print();
+        setTimeout(function () {window.close();}, 1);
+        cb();
+      }, 1000);
         
-        const url = '';
-        const win = openWindow(url, name, specs);
+      return true;
+    };
+  },
+};
   
-        win.document.write(`
-          <html>
-            <head>
-              <title>${window.document.title}</title>
-            </head>
-            <body>
-              ${element.innerHTML}
-            </body>
-          </html>
-        `);
-  
-        addStyles(win, styles);
-        
-        setTimeout(() => {
-          win.document.close();
-          win.focus();
-          win.print();
-          setTimeout(function(){window.close();}, 1);
-          cb();
-        }, 1000);
-          
-        return true;
-      };
-    }
-  }
-  
-  export default VueHtmlToPaper;
+export default VueHtmlToPaper;
