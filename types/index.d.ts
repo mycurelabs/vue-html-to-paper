3206b1b92d6e2cbd5eb5799c7897f6ac2f0dabe2
yzx9 <yuan.zx@outlook.com>
2023-06-14 20:30:00 +0800

    type: add typescript support

---
+++ types/index.d.ts
@@ -0,0 +1,29 @@
+import { Plugin } from "vue"
+
+/**
+ * See https://www.w3schools.com/Jsref/met_win_open.asp for more information
+ */
+interface Options {
+  name?: string
+  specs?: string[]
+  styles?: string[]
+  replace?: boolean
+  autoClose?: boolean
+  windowTitle?: string
+}
+
+interface HtmlToPaper {
+  (
+    id: string | HTMLElement,
+    localOptions: Options,
+    callback?: () => void
+  ): Promise<void>
+}
+
+export const VueHtmlToPaper: Plugin<Options>
+
+declare module "vue" {
+  interface ComponentCustomProperties {
+    $htmlToPaper: HtmlToPaper
+  }
+}
